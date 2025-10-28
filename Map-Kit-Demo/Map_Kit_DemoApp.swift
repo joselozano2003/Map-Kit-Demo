e7407128800dfb1ce6a6d4fb8df149f313ae9b74
Jose Lozano <josecamilo.lozanocet@ucalgary.ca>
2025-10-28 15:14:51 -0600

    Initial Commit

---
+++ Map-Kit-Demo/Map_Kit_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Map_Kit_DemoApp.swift
+//  Map-Kit-Demo
+//
+//  Created by Jose Lozano on 10/28/25.
+//
+
+import SwiftUI
+
+@main
+struct Map_Kit_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
